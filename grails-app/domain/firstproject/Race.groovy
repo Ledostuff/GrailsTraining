cea325c3992fe77b0054fcd5f688d9e7f506bb42
ledostuff <martemjanov@martemjanov.lan.is>
2014-03-19 17:43:54 +0600

    Первый комит

---
+++ grails-app/domain/firstproject/Race.groovy
@@ -0,0 +1,18 @@
+package firstproject
+
+class Race {
+	
+	String name
+	Date startDateTime
+	String city
+	String state
+	Float distance
+	Float cost
+	Integer maxRunners = 100000
+	
+	static hasMany = [registrations : Registration,
+			sponsors : Sponsors]
+
+    static constraints = {
+    }
+}
